59276e852abf9e751115c53c05cdb85736140f4b
ihfazhillah <mihfazhillah@gmail.com>
2017-09-05 13:53:33 +0700

    add childrenClassName demo

---
+++ demos/demo3-childrenClassName/Demo.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import { Nestable } from '../../src/react-dnd-nestable';
+
+class Demo extends Component {
+  state = {
+    items: [
+      { id: 1, text: 'Item #1', children: [] },
+      { id: 2, text: 'Item #2', children: [] },
+      { id: 3, text: 'Item #3', children: [] },
+      { id: 4, text: 'Item #4', children: [{ id: 5, text: 'Item #5', children: [] }] }
+    ]
+  };
+
+  renderItem = ({ item }) => {
+    return <div style={ styles.item }>{ item.text }</div>;
+  };
+
+  updateItems = (newItems) => {
+    this.setState({ items: newItems });
+  };
+
+  componentDidMount(){
+    require("./demo.css")
+  }
+
+  render() {
+    return (
+      <Nestable
+        items={ this.state.items }
+        renderItem={ this.renderItem }
+        onUpdate={ this.updateItems }
+        childrenClassName="child"
+      />
+    );
+  }
+}
+
+var styles = {
+  item: {
+    marginBottom: 5,
+    padding: 10,
+    border: '1px solid #000',
+    background: '#fff'
+  },
+};
+
+export default DragDropContext(HTML5Backend)(Demo);
